3e611fe4eea121a2514ed122241df445d4d34d63
jon.tirsen <jon.tirsen@8e0bbcb8-412a-0410-80cd-fb9098366f15>
2007-02-25 01:04:11 +0000

    implemented multi-word queries, changed "word" to "term"

--- trunk/hitta/test/acts_as_full_text_searchable_test.rb
+++ trunk/hitta/test/acts_as_full_text_searchable_test.rb
@@ -10,9 +10,9 @@ class ActsAsFullTextSearchableTest < Test::Unit::TestCase
     @post = Post.create!(:title => 'Ruby number 1 programming language', :body => 'Blah blah blah')
   end
   
-  def test_can_create_word_index
+  def test_can_create_term_index
     assert_equal ["1", "blah", "language", "number", "programming", "ruby"], 
-      ActsAsFullTextSearchable::Word.find(:all).collect(&:word).sort
+      ActsAsFullTextSearchable::Term.find(:all).collect(&:term).sort
   end
   
   def test_can_do_full_text_search
@@ -34,8 +34,9 @@ class ActsAsFullTextSearchableTest < Test::Unit::TestCase
     assert_equal 1, Post.find_by_full_text_search('BlaH').size
   end
   
-  # def test_can_search_for_multiple_terms
-  #   assert_equal @post, Post.find_by_full_text_search('blah ruby').first
-  #   assert_equal 0, Post.find_by_full_text_search('yadda ruby').size
-  # end
+  def test_can_search_for_multiple_terms
+    assert_equal @post, Post.find_by_full_text_search('blah ruby').first
+    assert_equal 0, Post.find_by_full_text_search('yadda ruby').size
+    assert_equal 1, Post.find_by_full_text_search('blah blah').size
+  end
 end
